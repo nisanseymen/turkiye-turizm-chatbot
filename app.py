31b056f1a8f8e7030ca4e253bf1d7b338c8480bc
nisan seymen <nisansey@gmail.com>
2025-10-21 18:40:44 +0300

    Son import ve requirements düzeltmesi

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import streamlit as st
-from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
